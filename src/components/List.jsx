d7a6de70a117318cf336c38a33f22a541942390e
Sakib3602 <sakibsarker6969@gmail.com>
2024-03-26 13:26:46 +0600

    install react tabs

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,6 @@
 import { FaAngleDown } from "react-icons/fa6";
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 const List = () => {
   return (
     <div>
@@ -8,7 +9,11 @@ const List = () => {
       </div>
 
       <div className="dropdown ">
-        <div  tabIndex={0} role="button" className="btn bg-[#23BE0A] mt-7 text-white font-[600]">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn bg-[#23BE0A] mt-7 text-white font-[600]"
+        >
           Sort By <FaAngleDown></FaAngleDown>
         </div>
         <ul
@@ -22,6 +27,23 @@ const List = () => {
             <a>Item 2</a>
           </li>
         </ul>
+      </div>
+
+      {/* tabs */}
+      <div className="mt-8">
+        <Tabs>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
